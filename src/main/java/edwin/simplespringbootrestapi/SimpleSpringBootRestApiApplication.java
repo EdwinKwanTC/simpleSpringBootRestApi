1ecc2ce4598baa9422f53b7e5d1c85b456b5fa95
Edwin Kwan <edwinkwantc@gmail.com>
2025-01-05 12:43:55 +0800

    feat: first commit

---
+++ src/main/java/edwin/simplespringbootrestapi/SimpleSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package edwin.simplespringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edwin/simplespringbootrestapi/SimpleSpringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edwin.simplespringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
