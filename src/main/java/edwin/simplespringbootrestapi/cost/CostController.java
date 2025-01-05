29ffacd19fe6c9b706d847023900187ec94e9cc4
Edwin Kwan <edwin.kwan@welab.co>
2025-01-05 17:16:44 +0800

    fix: deleting unused line

--- src/main/java/edwin/simplespringbootrestapi/cost/CostController.java
+++ src/main/java/edwin/simplespringbootrestapi/cost/CostController.java
@@ -1,22 +1,19 @@
 package edwin.simplespringbootrestapi.cost;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class CostController {
     @GetMapping("/cost/{id}")
-    Cost getCostById(@PathVariable(value = "id") Long id) {
+    ResponseEntity<Cost> getCostById(@PathVariable(value = "id") Long id) {
 
         System.out.println("id" + " " + id.toString());
         Cost a = new Cost("edwin",100.0);
-        return a;
+        return new ResponseEntity<>(a, HttpStatus.OK);
     }
 
     @GetMapping("/costs")
